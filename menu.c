d4893cd872447c45c71f3f63c41bf21849135975
Paola Campos da Silva <paa.campossilva@gmail.com>
2026-05-27 16:48:26 -0400

    feat: adding menu

--- menu.c
+++ menu.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int menu() {
-    
-    return 0;
+    int op;
+
+    printf("\n// ----- // ----- // ÁRVORE B // ----- // ----- //\n");
+    printf("[1] - Buscar\n");
+    printf("[2] - Inserir\n");
+    printf("[9] - Finalizar\n");
+    printf("-------------------------------\n");
+    printf("Entre com a sua opção: ");
+    scanf("%d", &op);
+
+    printf("\n");
+
+    return op;
 }
