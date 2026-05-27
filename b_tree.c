de4386a46d7562dd182ee130be34348529a4a298
jp-huppes <jpharenale@gmail.com>
2026-05-27 16:39:03 -0400

    feature: some function signatures

---
+++ b_tree.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void new_node();
+void create_tree();
+void search_tree();
+void insert_tree();
+void split_child();
+void insert_child();
+void print_tree();
+void destroy_tree();
