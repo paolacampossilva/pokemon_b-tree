b4580ea2742e411aa0b6b8603e3248077e4838c3
jp-huppes <jpharenale@gmail.com>
2026-05-27 16:38:08 -0400

    add: node.c

---
+++ node.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STRING_SYZE = 13;
+
+struct node{
+    char **pokemon_array;
+    struct node *children;
+    
+
+}; typedef struct  node node;
